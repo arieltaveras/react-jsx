533ae435a055ac5994370d3c97d44bf0625b520a
arieltaveras <ariel.taveras7278@gmail.com>
2023-08-18 05:39:56 -0400

    worked on style for part 2

--- part-2/App.js
+++ part-2/App.js
@@ -1,10 +1,20 @@
 const App = () => (
     <div>
         <Tweet 
-        username="RealBartSimpso"
+        username="RealBartSimpson"
         name="Bart Simpson"
         date="08/16/2023"
         message="Eat my shorts Principle Skinner!!!" />
+        <Tweet 
+        username="ChefGordonRamsey"
+        name="Gordon Ramsey"
+        date="08/10/2023"
+        message="Just had the most amazing duck!" />
+        <Tweet 
+        username="IamIronMan"
+        name="Anthony Stark"
+        date="08/12/2023"
+        message="Made a new Element...FREE ENERGY FOR ALL!" />
     </div>
 )
 
--- part-2/Tweet.js
+++ part-2/Tweet.js
@@ -1,11 +1,13 @@
 const Tweet = (props) => {
     console.log(props);
     return (
+        
         <ul className="tweet-props">
             <li className="username">Username: {props.username}</li>
             <li className="date">Date of Tweet: {props.date}</li>
             <li className="name">Name: {props.name}</li>
             <li className="tweet">Tweet: {props.message}</li>
+            <img src="https://cdn-icons-png.flaticon.com/512/124/124021.png" alt="" />
         </ul>
     );
 };
